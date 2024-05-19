3f133b8134c6b8a35a568dc5ccd594835cc11f28
Harshal Mali <144597912+HARSHGit45@users.noreply.github.com>
2024-05-19 02:41:11 -0700

    day2-medlevel

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int reverse(int x) {
+
+        int rev=0;
+        while(x!=0)
+        { 
+            int d = x % 10;
+
+            if( (rev > INT_MAX/10) ||  (rev < INT_MIN/10)  )
+            {
+                return 0;
+
+            }
+
+            rev= rev * 10 + d;
+
+            x=x/10;
+
+
+
+        }
+
+        return rev;
+        
+    }
+};
