7ae7af9550df790ca889477e449f076a9958ac42
Harshal Mali <144597912+HARSHGit45@users.noreply.github.com>
2024-05-24 06:51:55 -0700

    day-7-medium-level

---
+++ 1283. Find the Smallest Divisor Given a Threshold.cpp
@@ -0,0 +1,60 @@
+
+int sm(vector<int>&  arr,int dn,int n)
+{
+	int sum=0;
+	for(int i=0;i<n;i++)
+	{
+		int d = ceil((double)arr[i]/(double)dn);
+		sum=sum+d;
+		
+		
+	}
+	
+	return sum;
+	
+}
+
+class Solution {
+
+
+public:
+
+
+
+
+    int smallestDivisor(vector<int>& arr, int k) {
+
+int n=arr.size();
+
+
+    int s=1;
+	int e = *max_element(arr.begin(),arr.end());
+	int mid=s+((e-s)/2);
+	int ans=0;
+	
+	while(s<=e)
+	{
+		int sn=sm(arr,mid,n);
+		
+		
+		
+		if(sn<=k){
+			ans=mid;
+			e=mid-1;
+		}
+		else
+		{
+			s=mid+1;
+			
+		}
+		
+		mid=s+((e-s)/2);
+		
+	}
+	
+	return ans;
+	
+	
+        
+    }
+};
